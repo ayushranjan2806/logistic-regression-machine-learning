7bd8035bec7aedf2b71be62df00f49271f91648d
ayushranjan2806 <84095016+ayushranjan2806@users.noreply.github.com>
2022-01-18 16:36:29 +0530

    Add files via upload

---
+++ logistic_regession/1.py
@@ -0,0 +1,35 @@
+
+# Python program to swap two variables
+
+
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+# def swap(x,y):
+#     temp = x
+#     x = y
+#     y = temp
+
+
+
+
+# print("The value of x after swapping:",)
+# print(type(swap(2,4)))
+a = 11
+b = 7
+def value(a,b):
+    temp = a
+    a = b
+    b = temp
+
+print(a)
+ # 7
+
+print(type(a))
+print(b) 
+i=int((input("enter a number:")),int(input("enter  vf")).split()
+
+print(type(i))
---
+++ logistic_regession/2.py
@@ -0,0 +1,4 @@
+i,y=input("erhe").split(" ")
+print(i)
+print(y)
+print(type(i))
